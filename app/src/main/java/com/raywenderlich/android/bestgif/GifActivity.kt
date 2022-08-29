1ff8cb8b02a34089e48dd02b71bbbbb88d15b696
Andrey Krashenitza <andrey13@yandex.ru>
2022-08-29 13:28:29 +0300

    p324

--- app/src/main/java/com/raywenderlich/android/bestgif/GifActivity.kt
+++ app/src/main/java/com/raywenderlich/android/bestgif/GifActivity.kt
@@ -1,43 +1,13 @@
-/*
- * Copyright (c) 2020 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * This project and source code may use libraries or frameworks that are
- * released under various Open-Source licenses. Use of those libraries and
- * frameworks are governed by their own individual licenses.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package com.raywenderlich.android.bestgif
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.raywenderlich.android.bestgif.networking.GiphyApi
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.addTo
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_gif.*
 
 class GifActivity : AppCompatActivity() {
@@ -50,6 +20,15 @@ class GifActivity : AppCompatActivity() {
 
     list.layoutManager = LinearLayoutManager(this)
     list.adapter = adapter
+
+    text_input
+      .textChanges()
+      .flatMapSingle { GiphyApi.searchForGifs(it) }
+      .onErrorReturnItem(listOf(GiphyGif(
+        "https://media.giphy.com/media/SQ24FpNRW9yRG/giphy.gif")))
+      .subscribeOn(Schedulers.io()) .observeOn(AndroidSchedulers.mainThread())
+      .subscribe { adapter.items = it }
+      .addTo(disposables)
   }
 
   override fun onDestroy() {
--- app/src/main/java/com/raywenderlich/android/bestgif/GiphyViewHolder.kt
+++ app/src/main/java/com/raywenderlich/android/bestgif/GiphyViewHolder.kt
@@ -31,7 +31,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package com.raywenderlich.android.bestgif
 
 import android.view.View
